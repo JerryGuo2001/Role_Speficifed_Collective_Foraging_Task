4199edaeaff3aa7cc3507689d155ce38f2b3f8f7
JerryGuo2001 <jianleg@uci.edu>
2026-06-05 13:51:54 -0700

    make agents move faster

--- js/main_phase.js
+++ js/main_phase.js
@@ -283,7 +283,7 @@
       enableObservationPhase = true,
       observationRoundsPerDemo = 5,
 
-      modelMoveMs = 250,
+      modelMoveMs = 100,
       humanIdleTimeoutMs = 10000,
         // NEW: multi-map support
       observationMapCsvs = null, // array of 3 csv paths for demos
